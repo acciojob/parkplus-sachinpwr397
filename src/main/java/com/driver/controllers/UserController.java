fd6179f696c4982e7474d8877e8f7ab0825042ec
sachinpwr397 <79899346+sachinpwr397@users.noreply.github.com>
2024-05-03 19:22:11 +0530

    Update UserController.java

--- src/main/java/com/driver/controllers/UserController.java
+++ src/main/java/com/driver/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.driver.controllers;
-import com.driver.services.impl.UserServiceImpl;
+
+import com.driver.models.User;
+import com.driver.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,19 +10,25 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
     @Autowired
-    UserServiceImpl userService;
+    private UserService userService;
+
     @PostMapping("/register")
-    public ResponseEntity<Void> registerUser(@RequestParam String name, @RequestParam String phoneNumber, @RequestParam String password){
+    public ResponseEntity<Void> registerUser(@RequestParam String name, @RequestParam String phoneNumber, @RequestParam String password) {
+        userService.registerUser(name, phoneNumber, password);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PutMapping("/update")
-    public ResponseEntity<User> updatePassword(@RequestParam Integer userId, @RequestParam String password){
+    public ResponseEntity<User> updatePassword(@RequestParam Integer userId, @RequestParam String password) {
+        User updatedUser = userService.updatePassword(userId, password);
         return new ResponseEntity<>(updatedUser, HttpStatus.OK);
     }
 
     @DeleteMapping("/delete")
-    public void deleteUser(@RequestParam Integer userId){
+    public ResponseEntity<Void> deleteUser(@RequestParam Integer userId) {
+        userService.deleteUser(userId);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
